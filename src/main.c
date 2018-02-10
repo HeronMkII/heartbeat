592988746a425b52c3dc342acb03bbec6f15e564
bonnieweng1T9 <tzuyun.weng.bonnie@gmail.com>
2018-02-10 14:44:13 -0500

    RX TX callback counter tested

--- src/main.c
+++ src/main.c
@@ -1,9 +1,15 @@
 #include <uart/uart.h>
 #include <uart/log.h>
 #include <can/can.h>
+#include <avr/pgmspace.h>
+#include <util/delay.h>
 
 #define F_CPU 8
-#include <util/delay.h>
+#define PROGMEM __ATTR_PROGMEM__
+
+//uint8_t savedData[1]; //New added line
+uint8_t savedEPSChildData = 0;
+uint8_t displayEPSChildData;
 
 void rx_callback(uint8_t*, uint8_t);
 void tx_callback(uint8_t*, uint8_t*);
@@ -37,34 +43,47 @@ mob_t tx_mob = {
 };
 
 void tx_callback(uint8_t* data, uint8_t* len) {
+    print("TX_CALLBACK TRIGGERED\n");
     *len = 1;
     data[0] = parent_counter;
     parent_counter += 1;
-    print("Parent counter incremented\n");
+    print("Tx-EPS set OBC parent counter : %d\n",parent_counter);
+    _delay_ms(100);
+
 }
 
 void rx_callback(uint8_t* data, uint8_t len) {
-    print("TX received!\n");
+    print("RX_CALLBACK TRIGGERED\n");
     if (len != 0) {
         child_counter = data[0];
-        print("child_counter: %d\n", child_counter);
+        print("RX-EPS received OBC child_counter: %d\n", child_counter);
+        //store into memory of this board (add const in front)
+        //uint8_t savedData PROGMEM = child_counter;
+        uint8_t savedEPSChildData PROGMEM = child_counter;
+        print("RX-EPS child_counter saved to flash drive: %d\n", savedEPSChildData);
+        //PGM_VOID_P savedDataTable[] PROGMEM = data;
+
     } else {
         print("No data\n");
     }
+    _delay_ms(100);
 }
 
 int main() {
+    print("RESET!!!");
     init_uart();
     init_can();
+
+    //restore data from flash
+    displayEPSChildData = pgm_read_word_near(&savedEPSChildData);
+    printf("Display data: %d\n", displayEPSChildData);
 
     init_rx_mob(&rx_mob);
 
     if (is_paused(&rx_mob)) {
-        print("WHAT??\n");
+        print("WHAT?? Mob is paused\n");
     }
-
     init_tx_mob(&tx_mob);
-
     while (1) {
         resume_mob(&tx_mob);
         while (!is_paused(&tx_mob)) {}
