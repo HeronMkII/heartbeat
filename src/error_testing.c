71be0e462c3cf1930d3c00572c0907a09417f55f
Jack Berezny <jackberezny@users.noreply.github.com>
2018-04-14 12:14:14 -0400

    Initial Commit

--- src/error_testing.c
+++ src/error_testing.c
@@ -1,5 +1,6 @@
 //Error checking module
-//This code should be used to ensure that error checking in heartbeat.c and robust
+//This code should be used to ensure that error checking in heartbeat.c
+//is consistent and robust
 uint8_t main(void){
 
 }
