45b91b713b9de8ad7fb74b4a2c5fdc0304c13116
bonnieweng1T9 <tzuyun.weng.bonnie@gmail.com>
2018-05-29 00:16:32 -0400

    added message object assignment but have small bug :(

--- src/heartbeat.c
+++ src/heartbeat.c
@@ -2,6 +2,7 @@
 
 void init_heartbeat();
 void assign_heartbeat_status();
+void assign_status_message_objects();
 
 void rx_callback(uint8_t*, uint8_t);
 void tx_callback(uint8_t*, uint8_t*);
@@ -19,6 +20,27 @@ uint8_t ssm_id = 0b11; //will be changed by each SSM
 //obc {0b00} eps {0b10} pay {0b01}
 
 uint8_t fresh_start = 1; //1 is true. 0 is false
+
+/*Status CAN Message*/
+/*Assume 8 bytes: (0)sending SSM (1) receiving SSM (2) message type (2 for
+heartbeat) (3) obc status (4) eps status (5) pay status (6) time stamp */
+mob_t status_rx_mob = {
+    .mob_num = 0,
+    .mob_type = RX_MOB,
+    .dlc = 8,
+    .id_tag = {  }, // ID of this nodes parent
+    .id_mask = { 0x00f },
+    .ctrl = default_rx_ctrl,
+    .rx_cb = rx_callback
+};
+
+mob_t status_tx_mob = {
+    .mob_num = 1,
+    .mob_type = TX_MOB,
+    .id_tag = { }, // ID of this nodes child*****
+    .ctrl = default_tx_ctrl,
+    .tx_data_cb = tx_callback
+};
 
 //Functions implementations
 void assign_heartbeat_status() {
@@ -44,9 +66,36 @@ void assign_heartbeat_status() {
     }
 }
 
+void assign_status_message_objects() {
+    switch(ssm_id){
+        case 0b00:
+            status_rx_mob.mob_num = 1;
+            //status_rx_mob.id_tag = {OBC_STATUS_RX_MOB_ID}; //gives error :(
+            status_tx_mob.mob_num = 0;
+            //status_tx_mob.id_tag = {OBC_STATUS_TX_MOB_ID};
+            break;
+        case 0b10:
+            status_rx_mob.mob_num = 0;
+            //status_rx_mob.id_tag = EPS_STATUS_RX_MOB_ID;
+            status_tx_mob.mob_num = 1;
+            //status_tx_mob.id_tag = EPS_STATUS_TX_MOB_ID;
+            break;
+        case 0b01:
+            status_rx_mob.mob_num = 0;
+            //status_rx_mob.id_tag = PAY_STATUS_RX_MOB_ID;
+            status_tx_mob.mob_num = 1;
+            //status_tx_mob.id_tag = PAY_STATUS_TX_MOB_ID;
+            break;
+        default:
+            print("INVALID SSM ID");
+            break;
+    }
+}
+
 void init_heartbeat() {
     //assign_heartbeat_status();
     assign_heartbeat_status();
+    assign_status_message_objects();
     if (eeprom_read_dword((uint32_t*) INIT_WORD_EEMEM) != DEADBEEF){
         print("SSM FRESH START\n");
         eeprom_update_dword((uint32_t*) INIT_WORD_EEMEM, DEADBEEF);
@@ -78,27 +127,13 @@ void init_heartbeat() {
     }
 }
 
-mob_t status_rx_mob = {
-    .mob_num = 0,
-    .mob_type = RX_MOB,
-    .dlc = 1,
-    .id_tag = {  }, // ID of this nodes parent
-    .id_mask = { 0x00f },
-    .ctrl = default_rx_ctrl,
-    .rx_cb = rx_callback
-};
-
-mob_t status_tx_mob = {
-    .mob_num = 1,
-    .mob_type = TX_MOB,
-    .id_tag = { }, // ID of this nodes child*****
-    .ctrl = default_tx_ctrl,
-    .tx_data_cb = tx_callback
-};
-
+/*Status CAN Message*/
+/*Assume 8 bytes: (0)sending SSM (1) receiving SSM (2) message type (2 for
+heartbeat) (3) obc status (4) eps status (5) pay status (6) time stamp */
 void tx_callback(uint8_t* data, uint8_t* len) {
     *len = 8;
     data[0] = ssm_id;
+    //data[1] = 
     data[1] = 2; //message type: 2 for heartbeat
     data[2] = 0; //field number (follow CAN message format in OBC commnd queue)
     //data[3] = not finished
@@ -120,7 +155,7 @@ void rx_callback(uint8_t* data, uint8_t len) {
 
 //This main function simulates the example file in lib-common
 int main() {
-    ssm_id = 0b00;
+    ssm_id = 0b00; //obc
     init_heartbeat();
     //uint8_t test = *self_status;
     return 0;
--- src/heartbeat.h
+++ src/heartbeat.h
@@ -13,21 +13,21 @@
 //void assign_heartbeat_status();
 
 //CAN ids of each SSMs. Follow the convention of can_ids under lib-common
-#define OBC_STATUS_RX_MOB_ID {0X001c}
-#define OBC_STATUS_TX_MOB_ID {0X000b}
-#define EPS_STATUS_RX_MOB_ID {0X001b}
-#define EPS_STATUS_TX_MOB_ID {0X000a}
-#define PAY_STATUS_RX_MOB_ID {0X001a}
-#define PAY_STATUS_TX_MOB_ID {0X000c}
+#define OBC_STATUS_RX_MOB_ID 0x001c
+#define OBC_STATUS_TX_MOB_ID 0x000b
+#define EPS_STATUS_RX_MOB_ID 0x001b
+#define EPS_STATUS_TX_MOB_ID 0x000a
+#define PAY_STATUS_RX_MOB_ID 0x001a
+#define PAY_STATUS_TX_MOB_ID 0x000c
 
 //EEPROM address assignment to store status of each SSM
 //Address starts from 0x0000
 //Use const uint16_t to decalre the address value and type casted when using
 //eeprom functions.
 const uint16_t INIT_WORD_EEMEM  = 0x0000; //4 bytes
-const uint16_t OBC_STATUS_EEMEM = 0X0004; //1 byte
-const uint16_t EPS_STATUS_EEMEM = 0X0005; //1 byte
-const uint16_t PAY_STATUS_EEMEM = 0X0006; //1 byte
+const uint16_t OBC_STATUS_EEMEM = 0x0004; //1 byte
+const uint16_t EPS_STATUS_EEMEM = 0x0005; //1 byte
+const uint16_t PAY_STATUS_EEMEM = 0x0006; //1 byte
 
 #define DEADBEEF 0Xdeadbeef //4 bytes
 
@@ -42,6 +42,11 @@ extern uint8_t* child_status;
 extern uint8_t ssm_id; //will be changed by each SSM
 //obc {0x00} eps {10} pay {01}
 
-uint8_t fresh_start;
+extern uint8_t fresh_start;
+
+extern mob_t status_rx_mob;
+extern mob_t status_tx_mob;
+//extern mob_id_tag_t id_tag;
+
 
 #endif
